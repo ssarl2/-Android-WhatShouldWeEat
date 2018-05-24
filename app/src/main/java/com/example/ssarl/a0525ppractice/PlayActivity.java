0c59fb90e008a7a035573085758607a5c7746d9d
1180 <ssarl2@naver.com>
2018-05-25 02:59:22 +0900

    Android_SoftwareDevelope3_Test

---
+++ app/src/main/java/com/example/ssarl/a0525ppractice/PlayActivity.java
@@ -0,0 +1,13 @@
+package com.example.ssarl.a0525ppractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+    }
+}
